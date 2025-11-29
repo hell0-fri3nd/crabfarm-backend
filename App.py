feef86885a86b20126e9853f9a76c7ebf5735ed0
Fsociety-Mrn <lisboamillen30@gmail.com>
2025-11-29 18:58:37 +0800

    fix: add ip restriction, only allowred IP can access the endpoints

--- App.py
+++ App.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 from routers import Auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:7987",
+    "http://127.0.0.1:7987"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # or ["*"] for development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(Auth)
