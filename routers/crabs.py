942aec38e80ac30e481593c4bbe2c13a70239072
Fsociety-Mrn <lisboamillen30@gmail.com>
2026-01-12 22:59:22 +0800

    fiat: add view logs per logtype and crab id & deafult all

--- routers/crabs.py
+++ routers/crabs.py
@@ -86,25 +86,71 @@ async def insert_logs(request: Request, db: Session = Depends(get_db)):
     )
 
 
-@Crabs.get("/logs/{type}/{crab_id}")
+@Crabs.get("/logs/{log_type}/{crab_id}/") 
 @jwt_manager.requires_access
-async def insert_logs(
-        crab_id: int | None ,
-        type: str | None,
-        db: Session = Depends(get_db)
-    ): 
+async def view_logs(log_type: str, crab_id: int, request: Request, db: Session = Depends(get_db)): 
     
     try:
+        
         query = select(
-            CrabLogs
+            CrabLogs, Crab
         ).join(Crab, CrabLogs.crab_id == Crab.id)
         
         # OR conditions
         conditions = []
         if crab_id is not None:
             conditions.append(CrabLogs.crab_id == crab_id)
-        if type  is not None:
-            conditions.append(CrabLogs.type == type)
+        if log_type  is not None:
+            conditions.append(CrabLogs.type == log_type)
+        
+        if conditions:
+            query = query.where(or_(*conditions))
+            
+        crab_logs = db.execute(query).all()  # returns list of (CrabLogs, Crab)
+
+        if not crab_logs:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No logs found matching criteria")
+    
+        data = [
+            {
+                "log_id": log.id,
+                "crab_id": log.crab_id,
+                "type": log.type,
+                "width": float(log.width),
+                "weight": float(log.weight),
+                "created_at": log.created_at.isoformat(),
+                "crab_name": crab.name
+            }
+            for log, crab in crab_logs
+        ]
+        
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={
+                "status_code": status.HTTP_200_OK,
+                "detail": "Logs fetched successfully",
+                "data": data
+            }
+        )
+                
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+@Crabs.get("/logs/{log_type}") 
+@jwt_manager.requires_access
+async def view_all_logs(log_type: str, request: Request, db: Session = Depends(get_db)): 
+    
+    try:
+        
+        query = select(CrabLogs, Crab).join(Crab, CrabLogs.crab_id == Crab.id)
+        
+        # OR conditions
+        conditions = []
+        if log_type.lower() != "all":
+            conditions.append(CrabLogs.type == log_type)
         
         if conditions:
             query = query.where(or_(*conditions))
